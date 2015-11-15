6cbb6499af80022bb0abd5340dda6ed81f8580e3
Wrong Kendall <wrong-kendall@users.noreply.github.com>
2015-11-15 12:47:11 -0800

    Move structs out of each function. Clean up some Printfs. Create a function for getting a client. Create a function for reading the credentials file.

--- tank_utility.go
+++ tank_utility.go
@@ -16,6 +16,7 @@ import (
 //TODO(kendall): Separate library from script to dump info.
 //TODO(kendall): Replace Printf with logging and error/abort as appropriate.
 //TODO(kendall): Reduce duplication.
+//TODO(kendall): Add DeviceId to DeviceInfo struct.
 
 var insecure = flag.Bool("insecure", true, "Whether to skip certificate checks.")
 var credentials_file = flag.String("credentials_file", "", "Path to read username and pass from.")
@@ -28,32 +29,53 @@ type TankReading struct {
 	Time int64
 	TimeIso string
 }
+
 type Device struct {
 	Name string
 	Address string
 	Capacity int32
 	LastReading TankReading
 }
+
 type DeviceInfo struct {
 	Device Device
 }
 
-func GetToken(credentials_file string, tank_utility_endpoint string, insecure bool) string {
+type DeviceList struct {
+	Devices []string
+}
+
+type TokenResponse struct {
+	Token string
+}
+
+func readCredentialsFile(credentials_file string) (string, string) {
 	var credentials []byte
 	var err error
 	if credentials, err = ioutil.ReadFile(credentials_file); err != nil {
 		fmt.Printf("Could not read credentials file: %s\n", credentials_file)
 	}
 	credential_parts := strings.SplitN(strings.TrimSpace(string(credentials)), ":", 2)
+	return credential_parts[0], credential_parts[1]
+}
+
+func getHttpClient(insecure bool) (*http.Client) {
+	var client *http.Client
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
-	var client *http.Client
 	if insecure {
 		client = &http.Client{Transport: tr}
 	} else {
 		client = &http.Client{}
 	}
+	return client
+}
+
+func GetToken(credentials_file string, tank_utility_endpoint string, insecure bool) string {
+	var err error
+	user, password := readCredentialsFile(credentials_file)
+	client := getHttpClient(insecure)
 
 	path := []string{tank_utility_endpoint, "getToken"}
 	uri := strings.Join(path, "/")
@@ -62,18 +84,14 @@ func GetToken(credentials_file string, tank_utility_endpoint string, insecure bo
 		fmt.Printf("Request error: %s\n", req_err)
 	}
 	fmt.Printf("Using %s\n", uri)
-	req.SetBasicAuth(credential_parts[0], credential_parts[1])
+	req.SetBasicAuth(user, password)
 	resp, http_err := client.Do(req)
 	if http_err != nil {
 		fmt.Printf("Error: %s\n", http_err)
 	} else {
-		type Message struct {
-			Token string
-		}
-		var token_response Message
+		var token_response TokenResponse
 		var json_message []byte
 		json_message, err = ioutil.ReadAll(resp.Body)
-		fmt.Printf("Message: %s\n", json_message)
 		if err != nil {
 			fmt.Printf("Error: %s\n", err)
 		}
@@ -88,30 +106,18 @@ func GetToken(credentials_file string, tank_utility_endpoint string, insecure bo
 
 func GetDeviceList(token string, tank_utility_endpoint string, insecure bool) []string {
 	var err error
-	var client *http.Client
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	if insecure {
-		client = &http.Client{Transport: tr}
-	} else {
-		client = &http.Client{}
-	}
+	client := getHttpClient(insecure)
 
 	path := []string{tank_utility_endpoint, "devices"}
 	uri := strings.Join(path, "/") + "?token=" + token
-	fmt.Printf("Using %s\n", uri)
 	resp, http_err := client.Get(uri)
+
 	if http_err != nil {
 		fmt.Printf("Error: %s\n", http_err)
 	} else {
-		type Message struct {
-			Devices []string
-		}
-		var devices_response Message
+		var devices_response DeviceList
 		var json_message []byte
 		json_message, err = ioutil.ReadAll(resp.Body)
-		fmt.Printf("Message: %s\n", json_message)
 		if err != nil {
 			fmt.Printf("Error: %s\n", err)
 		}
@@ -126,20 +132,12 @@ func GetDeviceList(token string, tank_utility_endpoint string, insecure bool) []
 
 func GetDeviceInfo(device string, token string, tank_utility_endpoint string, insecure bool) DeviceInfo {
 	var err error
-	var client *http.Client
 	var device_response DeviceInfo
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-	if insecure {
-		client = &http.Client{Transport: tr}
-	} else {
-		client = &http.Client{}
-	}
+	client := getHttpClient(insecure)
 
 	path := []string{tank_utility_endpoint, "devices", device}
 	uri := strings.Join(path, "/") + "?token=" + token
-	fmt.Printf("Using %s\n", uri)
+
 	resp, http_err := client.Get(uri)
 	if http_err != nil {
 		fmt.Printf("Error: %s\n", http_err)
@@ -161,9 +159,7 @@ func GetDeviceInfo(device string, token string, tank_utility_endpoint string, in
 func main() {
 	flag.Parse()
 	token := GetToken(*credentials_file, *tank_utility_endpoint, *insecure)
-	fmt.Printf("Token: %s\n", token)
 	device_list := GetDeviceList(token, *tank_utility_endpoint, *insecure)
-	fmt.Printf("Devices: %s\n", device_list)
 	for i := 0; i < len(device_list); i++ {
 		var device_info DeviceInfo
 		device_info = GetDeviceInfo(device_list[0], token, *tank_utility_endpoint, *insecure)
