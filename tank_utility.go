490ebdea9835849290aa59d625c63b466670588e
Wrong Kendall <wrong-kendall@users.noreply.github.com>
2015-11-14 23:31:14 -0800

    Add support for grabbing the device information in addition to the list of devices.

--- tank_utility.go
+++ tank_utility.go
@@ -14,6 +14,22 @@ var credentials_file = flag.String("credentials_file", "", "Path to read usernam
 var output_token_file = flag.String("output_token_file", "tank_utility.token", "Path to write the token to.")
 var tank_utility_endpoint = flag.String("tank_utility_endpoint", "https://data.tankutility.com/api", "API endpoint for Tank Utility")
 
+type TankReading struct {
+	Tank float64
+	Temperature float32
+	Time int64
+	TimeIso string
+}
+type Device struct {
+	Name string
+	Address string
+	Capacity int32
+	LastReading TankReading
+}
+type DeviceInfo struct {
+	Device Device
+}
+
 func GetToken(credentials_file string, tank_utility_endpoint string, insecure bool) string {
 	var credentials []byte
 	var err error
@@ -100,10 +116,49 @@ func GetDeviceList(token string, tank_utility_endpoint string, insecure bool) []
 	return make([]string, 0)
 }
 
+func GetDeviceInfo(device string, token string, tank_utility_endpoint string, insecure bool) DeviceInfo {
+	var err error
+	var client *http.Client
+	var device_response DeviceInfo
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	if insecure {
+		client = &http.Client{Transport: tr}
+	} else {
+		client = &http.Client{}
+	}
+
+	path := []string{tank_utility_endpoint, "devices", device}
+	uri := strings.Join(path, "/") + "?token=" + token
+	fmt.Printf("Using %s\n", uri)
+	resp, http_err := client.Get(uri)
+	if http_err != nil {
+		fmt.Printf("Error: %s\n", http_err)
+	} else {
+		var json_message []byte
+		json_message, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+		err = json.Unmarshal(json_message, &device_response)
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+		}
+	}
+	return device_response
+}
+
 
 func main() {
 	flag.Parse()
 	token := GetToken(*credentials_file, *tank_utility_endpoint, *insecure)
 	fmt.Printf("Token: %s\n", token)
-	fmt.Printf("Devices: %s", GetDeviceList(token, *tank_utility_endpoint, *insecure))
+	device_list := GetDeviceList(token, *tank_utility_endpoint, *insecure)
+	fmt.Printf("Devices: %s\n", device_list)
+	for i := 0; i < len(device_list); i++ {
+		var device_info DeviceInfo
+		device_info = GetDeviceInfo(device_list[0], token, *tank_utility_endpoint, *insecure)
+		fmt.Printf("%#v\n", device_info)
+	}
 }
